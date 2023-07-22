40a6228fb5d70dd10689cb271bb05470e9056f96
IreneChicca <irene.chicca@outlook.com>
2023-07-22 14:38:46 +0200

    Style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 
-
-
-// Imposto un ciclo secondo un indice che va da 0 a 100, Ad ogni giro del ciclo stamperò nella console un numero che si incrementerà di volta in volta
-
 const myContainer = document.querySelector(".my-container")
+
+
 
 
 for (let i = 1; i <= 100; i++) {
@@ -11,26 +9,49 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
 
-        myContainer.innerHTML += "FIZZ BUZZ<br>"
+        const myBox = document.createElement('div');
+        myBox.innerHTML += "FIZZ BUZZ";
+        myBox.className = 'p-4 border m-3 rounded col-2 text-center fw-bold'
+        myBox.style.backgroundColor = '#66ffff'
 
+        myContainer.appendChild(myBox)
     }
 
 
     else if (i % 3 == 0) {
-        myContainer.innerHTML += "FIZZ <br>"
+        const myBox = document.createElement('div');
+        myBox.innerHTML += "FIZZ";
+        myBox.className = 'p-4 border m-3 rounded col-2 text-center fw-bold'
+        myBox.style.backgroundColor = '#ff99cc'
+
+        myContainer.appendChild(myBox)
     }
 
-    else if (i % 5 == 0)
+    else if (i % 5 == 0) {
 
-        myContainer.innerHTML += "BUZZ <br>"
+        const myBox = document.createElement('div');
+        myBox.innerHTML += "BUZZ";
+        myBox.className = 'p-4 border m-3 rounded col-2 text-center fw-bold'
+        myBox.style.backgroundColor = '#6600ff'
+
+        myContainer.appendChild(myBox)
 
 
+
+
+
+    }
 
 
     else {
 
+        const myBox = document.createElement('div');
+        myBox.innerHTML += i;
+        myBox.className = 'p-4 border m-3 rounded col-2 text-center fw-bold'
+        myBox.style.backgroundColor = '#99ccff'
 
-        myContainer.innerHTML += "giro" + i + "<br>"
+        myContainer.appendChild(myBox)
+
     }
 
 }
