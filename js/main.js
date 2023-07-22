7e1fcdfac400335577b41010565a2b70c12f3e5d
IreneChicca <irene.chicca@outlook.com>
2023-07-22 13:34:22 +0200

    JS console

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+// Imposto un ciclo secondo un indice che va da 0 a 100, Ad ogni giro del ciclo stamperò nella console un numero che si incrementerà di volta in volta
+
+for (let i = 0; i <= 100; i++) {
+
+    console.log(i);
+
+
+}
+
+
+
+
